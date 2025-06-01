ef600dc42c1b67cb2643c195da53157bea47243e
Syed Md Mihan Chistie <tubbachistie@gmail.com>
2025-06-02 02:11:21 +0530

    fix: fixed errors

--- next.config.js
+++ next.config.js
@@ -1,13 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  webpack: (config) => {
-    config.resolve.fallback = { fs: false, net: false, tls: false };
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        fs: false,
+        net: false,
+        tls: false,
+        crypto: false
+      };
+    }
     return config;
   },
-  // Enable SWC
   experimental: {
     forceSwcTransforms: true,
+    esmExternals: 'loose'
   }
 }
 
